c0d59ac01ef21cddab4180b5efa6c5ea4abc6b3e
Gaby <137128787+Gaby167@users.noreply.github.com>
2024-09-04 21:46:16 +0000

    Ej4 con cambios

--- labo_2_ej_4.py
+++ labo_2_ej_4.py
@@ -1,22 +1,16 @@
-import RPi.GPIO as GPIO
-import time
-
-# Setup GPIO
-GPIO.setmode(GPIO.BCM)
+from gpiozero import LED, Button
+from time import sleep
 
 # Define GPIO pins
-LED_PINS = [17, 27, 22, 5]
-BUTTON_1 = 6
-BUTTON_2 = 13
+leds = [
+    LED(17),  # LED 1 en el pin GPIO 17
+    LED(27),  # LED 2 en el pin GPIO 27
+    LED(22),  # LED 3 en el pin GPIO 22
+    LED(23),  # LED 4 en el pin GPIO 23
+]
 
-# Setup for LEDs
-for pin in LED_PINS:
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, GPIO.LOW)
-
-# Setup for buttons
-GPIO.setup(BUTTON_1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(BUTTON_2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+button_1 = Button(18, pull_up=True)  # Botón en GPIO 18 con pull-up
+button_2 = Button(15, pull_up=True) 
 
 # State management
 def initial_state():
@@ -24,12 +18,14 @@ def initial_state():
 
 # Button 1 press
 def button_1_press(state):
-    new_led = (state['selected_led'] + 1) % len(LED_PINS)
+    new_led = (state['selected_led'] + 1) % len(leds)
     return {'selected_led': new_led, 'duration': 1}
 
 # Button 2 press
 def button_2_press(state):
     new_duration = state['duration'] + 1
+    if new_duration > 10:
+        new_duration = 1
     return {'selected_led': state['selected_led'], 'duration': new_duration}
 
 # Activate LED
@@ -37,22 +33,23 @@ def activate_led(state):
     selected_led = state['selected_led']
     duration = state['duration']
     
-    GPIO.output(LED_PINS[selected_led], GPIO.HIGH)
-    time.sleep(duration)
-    GPIO.output(LED_PINS[selected_led], GPIO.LOW)
+    leds[selected_led].on()
+    sleep(duration)
+    leds[selected_led].off()
+    sleep(duration)
 
 # Main program loop
 def main():
     state = initial_state()
 
     while True:
-        if GPIO.input(BUTTON_1) == GPIO.LOW:
+        if button_1_press:
             state = button_1_press(state)
-            time.sleep(0.2)  # Debounce delay
+            sleep(0.2)  # Debounce delay
 
-        if GPIO.input(BUTTON_2) == GPIO.LOW:
+        if button_2_press:
             state = button_2_press(state)
-            time.sleep(0.2)  # Debounce delay
+            sleep(0.2)  # Debounce delay
 
         # Activate selected LED
         activate_led(state)
