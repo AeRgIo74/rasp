7a65b89b8ee3a617ce8322d6dc8ff8a6d92ca0e7
AeRgIo74 <104013708+AeRgIo74@users.noreply.github.com>
2024-09-09 04:13:37 -0400

    Create lb4e1.py

---
+++ lb4e1.py
@@ -0,0 +1,12 @@
+from gpiozero import Buzzer, Button
+from time import sleep
+
+buzzer = Buzzer(18)  # Define el pin para el buzzer
+button = Button(23)  # Define el pin para el botón
+
+while True:
+    if button.is_pressed:  # Si el botón está presionado
+        buzzer.on()  # Enciende el buzzer
+    else:
+        buzzer.off()  # Apaga el buzzer
+    sleep(0.1)  # Añade un pequeño retardo para evitar sobrecargar la CPU
