c747d7528108b027810bb6ab6312b39dbc03ab1c
AeRgIo74 <aergio74@gmail.com>
2024-09-10 03:22:49 -0400

    niufnds

--- pruebita.py
+++ pruebita.py
@@ -1,21 +1,13 @@
-import RPi.GPIO as GPIO
-import time
+from gpiozero import Buzzer, Button
+from time import sleep
 
-# Configurar la numeración de pines
-GPIO.setmode(GPIO.BCM)
+buzzer = Buzzer(18)  # Define el pin para el buzzer
+button = Button(23)  # Define el pin para el botón
 
-# Definir el pin al que está conectado el LED
-LED_PIN = 17
+while True:
+    if button.is_pressed:  # Si el botón está presionado
+        buzzer.on()  # Enciende el buzzer
+    else:
+        buzzer.off()  # Apaga el buzzer
+    sleep(0.1)  # Añade un pequeño retardo para evitar sobrecargar la CPU
 
-# Configurar el pin del LED como salida
-GPIO.setup(LED_PIN, GPIO.OUT)
-
-try:
-    while True:
-        GPIO.output(LED_PIN, GPIO.HIGH)  # Encender el LED
-        time.sleep(1)  # Esperar 1 segundo
-        GPIO.output(LED_PIN, GPIO.LOW)   # Apagar el LED
-        time.sleep(1)  # Esperar 1 segundo
-
-except KeyboardInterrupt:
-    GPIO.cleanup()  # Limpiar los pines al salir
